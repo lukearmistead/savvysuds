10bf13efa6ce99bb24e20c36f5bf3325e95c5a0c
Luke Armistead <armistead.luke@gmail.com>
2016-03-07 11:29:45 -0800

    web app debugging

--- savvysuds/web_app.py
+++ savvysuds/web_app.py
@@ -1,45 +1,30 @@
 import pandas as pd
 import graphlab as gl
-from flask import Flask
-from flask import render_template
-from flask import Flask, request
-from flask import g
-from flask import jsonify
+from flask import Flask, g, render_template, request
+
+
 app = Flask(__name__)
 
-'''
-todo
-- add autocomplete to find beers you want
-- replace weird characters in beer names
-- look into sudo command to get gl running
-- clean up ui
-'''
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
-# @app.route("/suggest")
-# def suggest():
-#     user_input = request.args.get('input')
-#     return jsonify({'asdf': 'you typed ' + user_input})
-    # implement python search of database
-    # return jsonified list of records
 
 @app.route("/recommend", methods=['POST'])
 def recommend():
-    brew1 = int(request.form['beer_input1_id'])
-    brew2 = int(request.form['beer_input2_id'])
-    brew3 = int(request.form['beer_input3_id'])
+    input_boxes = ['beer_input1_id', 'beer_input2_id', 'beer_input3_id']
+    user_input = []
+    for box in input_boxes:
+        submission = request.form[box]
+        if submission != '':
+            submission = int(submission)
+            user_input.append(submission)
 
-    user_input = [brew1, brew2, brew3]
-
-    user_input = pd.DataFrame({'a': [20000 for _ in user_input], 'b': user_input})
-    user_input.columns = ['user_id', 'item_id']
+    user_input = pd.DataFrame({'user_id': [20000 for _ in user_input], 'item_id': user_input})
     user_input = gl.SFrame(user_input)
     model = gl.load_model('../models/item_similarity_model')
     pred = list(model.recommend(users=[20000], k=5, new_observation_data=user_input, diversity=3)['item_id'])
-
     beers = pd.read_csv('../data/raw_data/beers_3.csv')
     beer_recs = beers[beers['id'].isin(pred)]
     beer_recs = beer_recs[['name', 'brewery_name', 'style', 'score']]
@@ -53,10 +38,8 @@ def recommend():
 
 
 def main():
-    # with app.app_context():
-        # app.g.beers = pd.read_csv('../data/raw_data/beers.csv')
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
