234bd6752904f0398c7a81e70422c5abbd8d7f58
Luke Armistead <armistead.luke@gmail.com>
2016-03-21 14:47:22 -0700

    tweaked recommender

--- savvysuds/web_app.py
+++ savvysuds/web_app.py
@@ -13,6 +13,7 @@ def main():
 
 @app.route("/recommend", methods=['POST'])
 def recommend():
+    # get user input
     input_boxes = ['beer_input1_id', 'beer_input2_id', 'beer_input3_id']
     user_input = []
     for box in input_boxes:
@@ -20,18 +21,25 @@ def recommend():
         if submission != '':
             submission = int(submission)
             user_input.append(submission)
-
     user_input = pd.DataFrame({'user_id': [20000 for _ in user_input], 'item_id': user_input})
-    user_input = gl.SFrame(user_input)
+
+    # identify user-favored styles for additional filtering
+    beers = pd.read_csv('../data/input/beers.csv')
+    user_styles = user_input.merge(beers, left_on='item_id', right_on='id')['style']
+    items = beers[beers['style'].isin(list(user_styles))]
+    items = gl.SFrame(items)['id']
+
+    # load model and generate recommendations
     model = gl.load_model('../models/item_similarity_model')
-    pred = list(model.recommend(users=[20000], k=5, new_observation_data=user_input, diversity=3)['item_id'])
-    beers = pd.read_csv('../data/raw_data/beers_3.csv')
+    user_input = gl.SFrame(user_input)
+    pred = list(model.recommend(users=[20000], items=items, k=5, new_observation_data=user_input, diversity=3)['item_id'])
+
+    # format recommendations for output
     beer_recs = beers[beers['id'].isin(pred)]
     beer_recs = beer_recs[['name', 'brewery_name', 'style', 'score']]
     beer_recs.columns = ['brew', 'brewery', 'style', 'untappd score']
     beer_recs = beer_recs.to_html(columns=['brew', 'brewery', 'style', 'untappd score'],
                                   index=False)
-
     beer_recs = beer_recs.replace('border="1" class="dataframe"',
                                   'class=table table-hover')
     return render_template('index.html', recommend=True, beer_recs=beer_recs)
